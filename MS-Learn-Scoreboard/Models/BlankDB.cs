f8bea7bca65ea2ebe85ea01109831ff4628acf22
David Lin <voidnullbot@outlook.com>
2020-04-26 17:23:35 +1200

    Project cleanup

---
+++ MS-Learn-Scoreboard/Models/BlankDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MS_Learn_Scoreboard.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS_Learn_Scoreboard.Models
+{
+    public class InitDatabase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MS_Learn_ScoreboardContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MS_Learn_ScoreboardContext>>()))
+            {
+                context.SaveChanges();
+            }
+        }
+ 
+    }
+}
