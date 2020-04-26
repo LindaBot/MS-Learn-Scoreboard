b120f822cd06ba46388f87a5052ebe9b3eb767ff
David Lin <voidnullbot@outlook.com>
2020-04-26 18:02:50 +1200

    Added Swagger UI and set as default page

--- MS-Learn-Scoreboard/Startup.cs
+++ MS-Learn-Scoreboard/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using MS_Learn_Scoreboard.Data;
+using Microsoft.OpenApi.Models;
 
 namespace MS_Learn_Scoreboard
 {
@@ -31,6 +32,12 @@ namespace MS_Learn_Scoreboard
 
             services.AddDbContext<MS_Learn_ScoreboardContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("MS_Learn_ScoreboardContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MS Learn XP API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +47,18 @@ namespace MS_Learn_Scoreboard
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MS Learn XP API");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
